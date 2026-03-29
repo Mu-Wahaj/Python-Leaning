19c1020a4b5bb9d651e7b98c8d909e0f97af0954
Muhammad Wahaj <mwahajjutt123@gmail.com>
2026-03-30 00:58:24 +0500

    Arithemtic Opertors

---
+++ new/arithmetic.py
@@ -0,0 +1,32 @@
+# Arithemtc operations in python
+# Addition
+a = 10
+b = 5
+addition = a + b
+print("Addition:", addition)
+# Subtraction
+subtraction = a - b
+print("Subtraction:", subtraction)
+# Multiplication
+multiplication = a * b
+print("Multiplication:", multiplication)
+# Division
+division = a / b
+print("Division:", division)
+# Floor Division
+floor_division = a // b
+print("Floor Division:", floor_division)
+# Modulus
+modulus = a % b
+print("Modulus:", modulus)
+# Exponentiation
+exponentiation = a ** b ## a raised to the power of b > a^b>10^5
+print("Exponentiation:", exponentiation)
+
+# increment and decrement
+a += 1  # Increment a by 1
+print("Incremented a:", a)
+b -= 1  # Decrement b by 1
+print("Decremented b:", b)
+# it does not change the original value of a and b, it returns a new value and assign it to a and b respectively    
+print("a + b =", a + b)
